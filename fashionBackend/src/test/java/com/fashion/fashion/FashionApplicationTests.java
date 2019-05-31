4deb9e561d16641390331314523af059bed64a17
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-05-31 23:41:51 +0530

    Initialize backend

---
+++ fashionBackend/src/test/java/com/fashion/fashion/FashionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fashion.fashion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FashionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
